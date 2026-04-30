d37cb1f6277ea69814e5c6a6a3c8b80f95a7eae0
Ubuntu <ubuntu@srv1580390.hstgr.cloud>
2026-04-30 18:59:03 +0000

    chore: cleanup legacy dev scripts and monitors from root

--- patch_trigger.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-const file = "src/index.js";
-let code = fs.readFileSync(file, "utf8");
-
-// Decrease threshold from 90 to 75
-code = code.replace(/const isExtreme = \(pLongPct >= 90 \|\| pShortPct >= 90\);/g, "const isExtreme = (pLongPct >= 75 || pShortPct >= 75);");
-code = code.replace(/const extremeLong = pLongPct >= 90;/g, "const extremeLong = pLongPct >= 75;");
-
-fs.writeFileSync(file, code);
