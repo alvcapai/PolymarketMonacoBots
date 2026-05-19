e8f972adea0c399523ca914ebee105a5284411d6
Álvaro Capai <alvaro.a.capai@gmail.com>
2026-05-19 17:58:50 -0300

    cleanup

--- check_tx.cjs
+++
@@ -1,8 +0,0 @@
-const { ethers } = require('ethers');
-const provider = new ethers.JsonRpcProvider('https://polygon-bor-rpc.publicnode.com');
-async function run() {
-  const receipt = await provider.getTransactionReceipt('0xa6721f5f024c4a55fb6a36c481f1f6979787792ec8a11195914644be406445c9');
-  console.log('Status:', receipt.status);
-  console.log('Logs:', receipt.logs.length);
-}
-run();
--- test-trade-2.js
+++
@@ -1,4 +0,0 @@
-import { executeTrade } from './src/trade/executor.js';
-import { Side } from '@polymarket/clob-client-v2';
-const token = '63950897065960821990436367120879712678981772102861122988611727179831934129000';
-executeTrade(token, Side.BUY, 2, 0.50, 50).then(console.log).catch(console.error);
